4a2d314728f812791ef6f8a369ab26e2662aee28
Praneeth Borlam <praneeth@incubyte.co>
2025-09-03 16:41:08 +0530

    organize components into proper folder structure with CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from './components/Header';
-import UserCard from './components/UserCard';
+import Header from './components/common/Header';
+import UserCard from './components/user/UserCard';
 import './App.css';
 
 function App() {
--- src/components/user/UserCard/UserCard.js
+++ src/components/user/UserCard/UserCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './UserCard.css';
 
 function UserCard({ name, email, role }) {
   return (
